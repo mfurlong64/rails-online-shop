242ea4dc2858c6b27a0280b6bae76fe66fe005b5
maxwellworkman & Michael Furlong <pair+maxwellworkman.mfurlong64@devbootcamp.com>
2016-07-11 16:39:29 -0700

    create a create route for Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,24 @@
 class ProductsController < ApplicationController
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
 
   end
 
   def create
-    render plain: params[:product].inspect
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description)
   end
 
 end
