4fcbdcf7ad4e2ef14f0d80f9f3d6c46b12a1365c
maxwellworkman & Michael Furlong <pair+maxwellworkman.mfurlong64@devbootcamp.com>
2016-07-11 17:48:36 -0700

    add routing in for edit calls

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,20 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @product = Product.new(product_params)
 
