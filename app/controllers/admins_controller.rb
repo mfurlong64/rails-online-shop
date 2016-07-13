105b1eb80ffabb476acb7a5bf0839fd8890eebeb
maxwellworkman & Michael Furlong <pair+maxwellworkman.mfurlong64@devbootcamp.com>
2016-07-13 09:42:42 -0700

    add routes as an admin to a NEW and EDIT page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,14 +1,56 @@
 class AdminsController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "secret"  #, except: :index
+  http_basic_authenticate_with name: "admin", password: "secret"
 
   def index
     @products = Product.all
     render "admin/index"
   end
 
-  def new
-
+ def show
+    @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+    render 'admin/new'
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render 'admin/edit'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :photo, :stock, :description, :price)
+  end
 
 end
